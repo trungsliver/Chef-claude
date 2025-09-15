bd8ecc98f7b9460f8015cce9b91a7cf4d46ea2b4
trungsliver <buiductrung.cva@gmail.com>
2025-09-15 09:34:00 +0700

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+        <div className="logo-container">
+            <img src="https://img.pikbest.com/png-images/20241030/iconic-chef-hat-logo_11027326.png!sw800" className="logo" alt="Vite logo" />
+            <p className="logo-name">Chef Claude</p>
+        </div>
+    </header>
+  );
+}
