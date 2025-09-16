23756d6dacafca6cd523d48911c63c8b8ae8cc46
trungsliver <buiductrung.cva@gmail.com>
2025-09-16 09:31:05 +0700

    toggling state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,27 +3,40 @@ import React from 'react'
 function Counter() {
   const [count, setCount] = React.useState(0);
 
-  function subtract(){
-    setCount(count - 1) 
-    setCount(count - 1) 
-    setCount(count - 1) 
+  const [goingOut, setGoingOut] = React.useState(false); 
+  // const btnGo = goingOut ? "Yes" : "No"; 
+
+
+  function subtract() {
+    setCount(prevCount => prevCount - 1)
   }
 
-  function add(){
-    setCount(prevCount => prevCount + 1) 
-    setCount(prevCount => prevCount + 1) 
-    setCount(prevCount => prevCount + 1) 
+  function add() {
+    setCount(prevCount => prevCount + 1)
+  }
+  
+  function toggleGoingOut() {
+    setGoingOut(prevGoingOut => !prevGoingOut)
   }
 
   return (
-    <main className="counter-container">
-      <h1>Count change</h1>
-      <div className="counter">
-        <button className="minus" onClick={subtract}>-</button>
-        <h2 className="count">{count}</h2>
-        <button className="plus" onClick={add}>+</button>
+    <div className='test-state'>
+      <div className="counter-container">
+        <h1>Count change</h1>
+        <div className="counter">
+          <button className="minus" onClick={subtract}>-</button>
+          <h2 className="count">{count}</h2>
+          <button className="plus" onClick={add}>+</button>
+        </div>
       </div>
-    </main>
+
+      <hr />
+
+      <div className="counter-container">
+        <h1>Going out</h1>
+        <button id="go-out" onClick={toggleGoingOut}>{goingOut ? "Yes" : "No"}</button>
+      </div>
+    </div>
   )
 }
 
