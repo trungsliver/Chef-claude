3a97bef40f7a2ebd11b2bc1ec983cfa6d63dff1a
trungsliver <buiductrung.cva@gmail.com>
2025-09-17 14:28:53 +0700

    form: radio button

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -6,35 +6,57 @@ export default function SignupForm() {
         const formData = new FormData(formEl);
         const email = formData.get("email");
         const password = formData.get("password");
-        console.log("form submitted: ", {email, password});
-        formEl.reset(); 
+        console.log("form submitted: ", { email, password });
+        formEl.reset();
     }
 
     function signUp(formData) {
-        const email = formData.get("email");    
+        const email = formData.get("email");
         const password = formData.get("password");
-        console.log("form submitted 2", {email, password});
+        console.log("form submitted 2", { email, password });
+        // radio buttons
+        const employmentStatus = formData.get("employmentStatus");
+        console.log("employmentStatus: ", employmentStatus);
     }
 
     return (
-    <section className="signup-form">
-      <h2>Signup form</h2>
-      <form action={signUp} onSubmit={signUpHandler}>
-        <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" defaultValue="trung@gmail.com"/>
-        <br />
+        <section className="signup-form">
+            <h2>Signup form</h2>
+            {/* <form action={signUp} onSubmit={signUpHandler}> */}
+            <form action={signUp} >
+                <label htmlFor="email">Email:</label>
+                <input id="email" type="email" name="email" placeholder="joe@schmoe.com" defaultValue="trung@gmail.com" />
+                <br />
 
-        <label htmlFor="password">Password:</label>
-        <input id="password" type="password" name="password" placeholder="must be at least 8 characters" />
-        <br />
+                <label htmlFor="password">Password:</label>
+                <input id="password" type="password" name="password" 
+                        placeholder="must be at least 8 characters" required />
+                <br />
 
-        <label htmlFor="description">Description:</label>
-        <textarea id="description" name="description" placeholder="Tell us about yourself"></textarea>  
-        <br />
+                <fieldset>
+                    <legend>Employment Status:</legend>
+                    <label>
+                        <input type="radio" name="employmentStatus" value="unemployed"/>
+                        Unemployed
+                    </label>
+                    <label>
+                        <input type="radio" name="employmentStatus" value="part-time" />
+                        Part-time
+                    </label>
+                    <label>
+                        <input type="radio" name="employmentStatus" value="full-time" defaultChecked={true}/>
+                        Full-time
+                    </label>
+                </fieldset>
+                <br />
 
-        <button type="submit">Create account</button>
-        
-      </form>
-    </section>
-  )
+                <label htmlFor="description">Description:</label>
+                <textarea id="description" name="description" placeholder="Tell us about yourself"></textarea>
+                <br />
+
+                <button type="submit">Create account</button>
+
+            </form>
+        </section>
+    )
 }
